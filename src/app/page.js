e2aa4099509b55783b60c1e51deb618be6473f18
Bacochina1 <bacochinamatheus@gmail.com>
2025-01-05 15:39:38 -0300

    fix: build

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 "use client";
-import Navbar from "./sections/navbar";
+import Navbar from "./sections/Navbar";
 import Hero from "./sections/HeroSection";
 export default function Home() {
   return (
